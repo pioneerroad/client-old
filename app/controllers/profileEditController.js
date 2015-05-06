12ff1c971ffb7a1e839869a48be9d97a45c06760
Piyush Chauhan <piyushchauhan2011@gmail.com>
2015-05-06 15:03:51 +1000

    HomeTown Search.

--- app/controllers/profileEditController.js
+++ app/controllers/profileEditController.js
@@ -1,10 +1,27 @@
 angular.module('pioneerRoadConnect')
   .controller('ProfileEditCtrl', ['$scope', function($scope) {
 
+    angular.element('#homeTownList').height(angular.element(window).height());
+    $scope.showHomeTown = false;
+    $scope.user = {};
+
+    $scope.listHomeTown = function() {
+      $scope.showHomeTown = true;
+    };
+    $scope.closeHomeTown = function() {
+      $scope.showHomeTown = false;
+    };
+
+    $scope.populateHomeTown = function(homeTown) {
+      $scope.hometownList = "";
+      $scope.user.hometown = homeTown;
+      $scope.closeHomeTown();
+    };
+
     // make change button as a directive
     $scope.btnText = {};
     $scope.btnClass = {};
-    $scope.fields = ['fullname', 'backgroundPhoto', 'profilePhoto', 'rvType', 'brandRv', 'introduction'];
+    $scope.fields = ['fullname', 'backgroundPhoto', 'profilePhoto', 'rvType', 'brandRv', 'introduction', 'hometown'];
 
     $scope.fields.forEach(function(field) {
       $scope.btnText[field] = 'Private';

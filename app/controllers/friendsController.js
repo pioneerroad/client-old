2d603f3dc19ad0125068874c660ecccf613ce5d4
Piyush Chauhan <piyushchauhan2011@gmail.com>
2015-05-21 09:51:49 +1000

    Added Friend Profile View.

--- app/controllers/friendsController.js
+++ app/controllers/friendsController.js
@@ -2,4 +2,11 @@ angular.module('pioneerRoadConnect')
   .controller('FriendsCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
     $scope.values = [1, 2, 3, 4];
     $rootScope.header = 'friends';
+    $scope.friendProfileView = false;
+    $scope.showFriendProfileView = function() {
+    	$scope.friendProfileView = true;
+    };
+    $scope.closeFriendProfileView = function() {
+      $scope.friendProfileView = false;
+    };
   }]);
